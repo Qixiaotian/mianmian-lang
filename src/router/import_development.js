ed8d39b567ae87fb28713e7fc07475a842a8f3e2
root705829 <3525088417@qq.com>
2019-08-30 08:45:13 +0800

    设置为自动打开浏览器，调整封装的路由函数

--- src/router/import_development.js
+++ src/router/import_development.js
@@ -1,1 +1,4 @@
-module.exports = file => require('@/module-' + file + '.vue').default // vue-loader at least v13.0.0+
+// module.exports = file => require('@/module-' + file + '.vue').default // vue-loader at least v13.0.0+
+module.exports = function (file) {
+    return require('@/module-' + file + '.vue').default
+}
